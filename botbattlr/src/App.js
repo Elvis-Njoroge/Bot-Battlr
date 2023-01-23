afe05aa2f3cf73e269e824e7bdf5857c503f29cd
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-01-23 23:23:29 +0300

    first commit

---
+++ botbattlr/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BotsPage from "./components/BotsPage";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BOT BATTLR</h1>
+      <BotsPage />
+    </div>
+  );
+}
+
+export default App;
+
